55f42eabd0018999a73e54b752333559a8d393a4
João Pedro <jpmeira020@gmail.com>
2022-05-05 18:32:25 -0300

    Commit 1

---
+++ src/main/java/org/generation/lojagames/model/Produto.java
@@ -0,0 +1,91 @@
+package org.generation.lojagames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "Titulo é obrigatório")
+	private String titulo; 
+	
+	@NotNull(message = "Produtora é obrigatório")
+	private String produtora;
+	
+	@NotNull(message = "Console é obrigatório")
+	private String console;
+	
+	@NotNull(message = "Preço é obrigatório")
+	@Positive
+	private BigDecimal preço; 
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getProdutora() {
+		return produtora;
+	}
+
+	public void setProdutora(String produtora) {
+		this.produtora = produtora;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public BigDecimal getPreço() {
+		return preço;
+	}
+
+	public void setPreço(BigDecimal preço) {
+		this.preço = preço;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
